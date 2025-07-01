b708f568e0e604053cd0ed7d4c351a123be89086
aycan58786 <aycan.guney14@gmail.com>
2025-07-01 12:27:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print('merhaba')
+internet_sozluk={
+    "LOL" : "Gülmek",
+    "AFK" : "Bilgisayardan uzak",
+    "TDK" : "Türk dil kurumu"
+}
+while True:
+    word = input("Bilemediğiniz kelimeleri yazınız:")
+    if word in internet_sozluk.keys():
+        print(internet_sozluk[word])
+    else:
+        print("Bu bu kelime sözlükte bulunamadı")
+
